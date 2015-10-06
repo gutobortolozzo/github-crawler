0173c16130ccddadb75970247c68c3583708b4e6
joao.bortolozzo <joao.bortolozzo@objective.com.br>
2015-10-06 17:12:32 -0300

    Rank test

---
+++ test/new_crawler/rank/rankTest.js
@@ -0,0 +1,31 @@
+require("should");
+var rank = require(process.cwd()+"/src/new_crawler/rank/rank");
+
+"use strict";
+describe('rank', function () {
+
+    const content = "<!DOCTYPE html>"+
+        "<html>"+
+        "<body>"+
+            "<p><a href=\"http://www.github.com.com/test/test\">Visit our HTML tutorial</a></p>"+
+        "</body>"+
+        "</html>";
+
+    it('extracted one referenced project', function () {
+        rank.calculateRank(content, "crawler", "crawler").length.should.be.eql(1);
+    });
+
+    it('extracted one referenced project and check owner/project', function () {
+        var ranking = rank.calculateRank(content, "crawler", "crawler");
+
+        ranking.length.should.be.eql(1);
+        ranking.pop().should.have.properties({
+            owner   : "test",
+            project : "test"
+        })
+    });
+
+    it('extracted none referenced project since owner/project is the same', function () {
+        rank.calculateRank(content, "test", "test").length.should.be.eql(0);
+    });
+});
